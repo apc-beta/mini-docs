3db0c8c49d1676aadb4532961a0b8e51a2a926db
Akshay <riteshchakrawarti2703@gmail.com>
2024-02-20 03:24:08 +0530

    Final touch added cards and drags.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-function Card() {
+function Card({ data, reference }) {
   return (
-    <div className='relative w-60 h-72 bg-zinc-900/90 rounded-[50px] text-white px-8 py-10 overflow-hidden'>
-        <FaRegFileAlt />
-        <p className='text-sm leading-tight font-semibold mt-5'>lorem iasdca asdkasfa dfa sfa sfasdfadf ad adfa sfasfa</p>
-        <div className='footer absolute bottom-0 w-full left-0 py-3 px-8'>
-            <div className='flex items-center justify-between'>
-                <h5>0.4mb</h5>
-                <LuDownload />
-            </div>
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.1 }}
+      dragTransition={{bounceDamping: 30, bounceStiffness: 100}}
+      className="relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white px-8 py-10 overflow-hidden"
+    >
+      <FaRegFileAlt />
+      <p className="text-sm leading-tight font-semibold mt-5">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex items-center justify-between py-3 px-8 mb-3">
+          <h5>{data.filesize}</h5>
+          <span className="w-7 h-7 rounded-full flex items-center justify-center bg-zinc-600">
+            {data.close ? <IoClose /> : <LuDownload size=".7em" color="#fff" />}
+          </span>
         </div>
-    </div>
-  )
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            } flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold"> {data.tag.tagTitle}</h3>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import Card from './Card'
-
+import {React, useRef} from "react";
+import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "The Data I want to display here is what is being displayed.",
+      filesize: ".9mb",
+      close: false,
+      tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"}
+    },
+    {
+      desc: "AK-47 is a nice gun to have an accomodation with and a seriously deadly weapon.",
+      filesize: ".4mb",
+      close: true,
+      tag: {isOpen: false, tagTitle: "Download Now", tagColor: "green"}
+    },
+    {
+      desc: "Mess around the card components a little too long seems like a game but not for kids very unhealthy.",
+      filesize: ".5mb",
+      close: false,
+      tag: {isOpen: true, tagTitle: "Upload", tagColor: "blue"}
+    },
+  ];
+  const ref = useRef(null);
   return (
-    <div className='fixed top-0 left-0 h-full w-full z-[3]'>
-        <Card />
+    <div ref = {ref} className="fixed top-0 left-0 h-full w-full z-[3] flex gap-10 flex-wrap p-5">
+      {data.map((item, index)=>(
+        <Card data ={item} key = {index} reference = {ref} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
